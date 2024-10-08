5083b2ff1d18df33acd8513dc456918276d882a0
Johnolin10 <johno1001741060168@gmail.com>
2024-10-08 16:24:49 +0800

    Implement dark theme toggle and CSS refactoring
    This update introduces a dark/light theme toggle feature in the Todo app, enhancing user experience particularly for varying lighting conditions. We added theme-related logic in App.js to store user theme preferences across sessions and applied it globally via document element attributes. To complement this functionality, we refactored CSS across several components (App.css, AddTodo.css, TodoList.css) and global styles (index.css, theme.css) to use CSS variables, facilitating easier theme changes and improving maintainability.
    
    Key changes include:
    - Addition of a theme toggle button in the header for switching between light and dark modes.
    - Introduction of CSS variables in stylesheets to manage colors, ensuring consistency and simplifying the switching between themes.
    - Adjustment in component layouts and styles to accommodate the new theme toggle feature and confirm to best practices in responsive design.
    
    These improvements aim not only to provide a better user experience but also to lay a foundation for future design and functionality enhancements within the app.
    
    References to specific issues or further plans:
    - Further accessibility improvements in theme switching are planned, with an eye on enhancing usability for users with specific needs.
    - Exploration of user-customizable themes has been suggested for a future update, aiming to provide an even more personalized experience.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /**
  * Todo App
  * @author John Lin
- * @version 1.0.0
+ * @version 1.3.0
  * @description A simple todo list app built with React and SCSS.
  */
 
@@ -11,12 +11,26 @@ import TodoList from './components/TodoList/TodoList'
 import AddTodo from './components/AddTodo/AddTodo'
 
 function App() {
+  // Todos
   const [todos, setTodos] = useState(() => {
     // Retrieve todos from local storage
     const savedTodos = localStorage.getItem('todos')
     return savedTodos ? JSON.parse(savedTodos) : []
   })
 
+  // Theme
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem('theme') || 'light'
+  })
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  // Refresh page
   const refreshPage = () => {
     window.location.reload(true)
   }
@@ -45,17 +59,25 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1 onClick={refreshPage}>
-          TODO
-          <span class="material-symbols-outlined check-icon">check</span>
-        </h1>
+        <div className="empty"></div>
+        <div className="logo">
+          <h1 onClick={refreshPage}>
+            TODO
+            <span class="material-symbols-outlined check-icon">check</span>
+          </h1>
+        </div>
+        <div className="theme-toggle">
+          <button onClick={toggleTheme}>
+            <span class="material-symbols-outlined">contrast</span>
+          </button>
+        </div>
       </header>
       <AddTodo addTodo={addTodo} />
       <TodoList todos={todos} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
       <div className="footer-container">
         <span>@John Lin</span>
         <span className="footer">
-          TODO v1.2.0{` `}
+          TODO v1.3.0{` `}
           <a
             href="https://github.com/johnlin10/to-do-list"
             target="_blank"
